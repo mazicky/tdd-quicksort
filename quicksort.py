fef05987dd09c10036ebbb055e63edd3ecd6a858
mazicky <mazicky@gmail.com>
2017-06-22 14:08:26 +0900

    implemented quick sort

--- quicksort.py
+++ quicksort.py
@@ -1,2 +1,26 @@
 def quicksort(input):
+    return _quicksort(input, 0, len(input)-1)
+
+def _quicksort(input, start, end):
+    if end <= start:
+        return
+
+    pivot = start
+    left = pivot+1
+    right = end
+
+    while left < right:
+        while input[left] < input[pivot] and left < end:
+            left += 1
+        while input[right] > input[pivot]:
+            right -= 1
+
+        if left < right and input[left] > input[right]:
+            input[left], input[right] = input[right], input[left]
+
+    if input[right] < input[pivot]:
+        input[right], input[pivot] = input[pivot], input[right]
+
+    _quicksort(input, start, right-1)
+    _quicksort(input, right+1, end)
     return input
--- test_quicksort.py
+++ test_quicksort.py
@@ -5,4 +5,7 @@ from quicksort import quicksort
 class Quicksort(unittest.TestCase):
     def test_length_2(self):
         self.assertEqual(quicksort([1 ,2]), [1, 2])
-
+        self.assertEqual(quicksort([2 ,1]), [1, 2])
+        self.assertEqual(quicksort([3, 2 ,1]), [1, 2, 3])
+        self.assertEqual(quicksort([5, 6, 3, 2 ,1]), [1, 2, 3, 5, 6])
+        self.assertEqual(quicksort([5, 6, 3, 10 , 1, 2]), [1, 2, 3, 5, 6, 10])
